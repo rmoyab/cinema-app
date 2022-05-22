96034fb954bfeafd8e0342daff8f624a6e3411e0
darsay8 <darsay.one@gmail.com>
2022-05-22 01:00:36 -0400

    server path fix

--- src/server/index.js
+++ src/server/index.js
@@ -1,15 +1,27 @@
 const path = require('path')
 const express = require('express')
-
+const cors = require('cors')
 require('dotenv').config()
+
+const publicPath = path.join(__dirname, '../..', 'dist')
 
 const app = express()
 
+app.use(cors())
 app.use(express.static('dist'))
 
 app.get('/', (req, res) => res.send('<h1>Root</h1>'))
 
 app.use(require('./routes/helloExpress'))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html')),
+    function (err) {
+      if (err) {
+        res.status(500).send(err)
+      }
+    }
+})
 
 const { env } = process
 const PORT = env.PORT || env.DEV_PORT

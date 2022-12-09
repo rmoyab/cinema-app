23506f20d79ee00d0b19fb4367df6017494b23b6
darsay8 <darsay.one@gmail.com>
2022-12-09 19:44:38 -0300

    fix ga

--- src/client/app/components/screen/LoginScreen.js
+++ src/client/app/components/screen/LoginScreen.js
@@ -2,6 +2,7 @@ import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { motion } from 'framer-motion'
 import { useNavigate, useLocation } from 'react-router-dom'
+import ReactGA from 'react-ga4'
 
 import { FiChevronLeft } from 'react-icons/fi'
 

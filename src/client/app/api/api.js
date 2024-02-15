0c23e1aa0c980ddd40785031e7459dd61c599e0f
darsay8 <darsay.one@gmail.com>
2024-02-15 18:51:41 -0300

    update

--- src/client/app/api/api.js
+++ src/client/app/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const userAxios = axios.create({
-  baseURL: process.env.baseURL || process.env.REACT_APP_BASE_URL,
+  baseURL: process.env.BASE_URL || process.env.REACT_APP_BASE_URL,
 })
 
 export const movieAxios = axios.create({
